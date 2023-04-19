ab00959d7d9470b0fcaa0562c4ec3c4acc6692c8
Britton Walker <bwalker1801@gmail.com>
2023-04-19 11:56:59 -0400

    Remove conditional wrapper

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,7 @@
 import GalleryUpload from "./gallery-upload";
-import ConditionalWrapper from "./conditional-wrapper";
 import PostSelector from "./post-selector";
 import CustomModal from "./modal";
 import ImageUpload from "./image-upload";
 import LinkPopover from "./link-popover";
 
-export {
-	GalleryUpload,
-	ConditionalWrapper,
-	CustomModal,
-	PostSelector,
-	ImageUpload,
-	LinkPopover,
-};
+export { GalleryUpload, CustomModal, PostSelector, ImageUpload, LinkPopover };
